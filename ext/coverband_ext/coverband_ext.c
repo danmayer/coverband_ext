b0955a96f5cb8a869fc0d9d3c16152cbaf5d8aef
Dan Mayer <dan.mayer@livingsocial.com>
2014-05-13 21:39:30 -0400

    this currently works.

--- ext/coverband_ext/coverband_ext.c
+++ ext/coverband_ext/coverband_ext.c
@@ -1,6 +1,6 @@
 /************************************************
 
-  coverband.c -
+  coverband_ext.c -
 
   $Author: $
 
@@ -9,17 +9,18 @@
 ************************************************/
 
 #include "ruby.h"
+
 static VALUE hello_world(VALUE mod)
 {
   return rb_str_new2("hello world");
 }
 
-void Init_coverband(void)
+void Init_coverband_ext()
 {
     /* VALUE rb_mCoverage = rb_define_module("Coverage"); */
     /* rb_define_module_function(rb_mCoverage, "start", rb_coverage_start, 0); */
     /* rb_define_module_function(rb_mCoverage, "result", rb_coverage_result, 0); */
     /* rb_gc_register_address(&rb_coverages); */
-    VALUE mCoverband = rb_define_module("Coverband");
-    rb_define_singleton_method(mCoverband, "hello_world", hello_world, 0);
+    VALUE mCoverbandExt = rb_define_module("CoverbandExt");
+    rb_define_singleton_method(mCoverbandExt, "hello_world", hello_world, 0);
 }
